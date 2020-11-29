8e450ab785bbe0b2ffe3aaa1b392918f56f7c6a7
cobrapitz <12397702+cobrapitz@users.noreply.github.com>
2020-11-29 15:37:27 +0100

    dock on the bottom right side of the editor, also changed column size to 1 bc of min size

--- addons/QuickSceneRunner/QuickSceneRunner.gd
+++ addons/QuickSceneRunner/QuickSceneRunner.gd
@@ -6,10 +6,12 @@ var dock: Control
 func _enter_tree():
 	dock = preload("res://addons/QuickSceneRunner/QuickSceneRunner.tscn").instance()
 	dock.plugin = self
-	add_control_to_bottom_panel(dock, "Quick Scenes")
+	#add_control_to_bottom_panel(dock, "Quick Scenes")
+	add_control_to_dock(DOCK_SLOT_LEFT_BR, dock)
 
 func _exit_tree():
-	remove_control_from_bottom_panel(dock)
+#	remove_control_from_bottom_panel(dock)
+	remove_control_from_docks(dock)
 	dock.free()
 
 func run_scene(scene: String):
